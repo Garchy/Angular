2d18e58d3c7ca1094e09a96997e69465be4e2f28
Garchy <byel20102010@gmail.com>
2022-05-27 16:33:12 -0300

    initial commit - first code with angular

---
+++ HelloWorld/src/app/first/first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-first-component',
+    template: `
+        <p>My First Component with Angular!</p>
+    `,
+})
+export class MyFirstComponent {
+    constructor() {
+        
+    }
+}
